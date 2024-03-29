c517dafe5522e9f1fc4e339f4abff6d7af18c23a
Sebastian Torreiro <sebastiantorreiro@gmail.com>
2024-03-29 12:21:25 -0300

    login, register & components

---
+++ src/app/login/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import { getServerSession } from "next-auth/next";
+// import { authOptions } from "@/libs/auth";
+// import { Session } from "next-auth";
+import { redirect } from 'next/navigation';
+import Signin from '@/components/account/Signin';
+
+const Login = async () => {
+//   const session: Session | null = await getServerSession(authOptions);
+    const session: boolean = false
+
+  if (session) {
+    redirect('/');
+  } else {
+    return (
+      <Signin />
+    )
+  }
+}
+
+export default Login;
