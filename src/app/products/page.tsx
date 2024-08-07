f6dde54138d0a6ade0ac751a82291736f54c5528
Sebastian Torreiro <sebastiantorreiro@gmail.com>
2024-08-06 23:46:24 -0300

    fix detail product

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,16 +1,16 @@
 import { Products } from "@/components/products/Products";
 import ProductSkeleton from "@/components/skeletons/ProductSkeleton";
-import Sidebar from "@/components/ui/sidebar";
+import Sidebar, { Filters } from "@/components/ui/sidebar";
 import { EnrichedProducts } from "@/types/types";
 import axios from "axios";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 
 // agregar filtros
 // necesito entender como al apretar un check, se agrega un filtro a la peticion
 // obviamente se como hacer un check y hacer un replace en la url
 //
 // agregar paginacion
-// agregar offset y limit y ver como sacar el numero max de pags para hacer los botones de la paginacion.
+// agregar offset y limit y ver como sacar el numero max de pags para hacer los  botones de la paginacion.
 
 interface SearchProps {
   searchParams: { [key: string]: string | undefined };
@@ -23,30 +23,76 @@ const normalizeText = (text: string): string => {
     .toLowerCase();
 };
 const Home: React.FC<SearchProps> = async ({ searchParams }) => {
-  const productsData = await axios.get("http://localhost:4000/product");
-  const products: EnrichedProducts[] = productsData.data;
+  // quiero ver si puedo regular la cantidad de items por pagina segun el ancho de la pantalla.
+  //
 
-  let filteredProducts: EnrichedProducts[] = [];
-  console.log("productos", products);
+  // // hecho con chat GPT. MEJORAR. INICIO
+  const params = new URLSearchParams();
+  (Object.keys(searchParams) as (keyof Filters)[]).forEach((key) => {
+    if (searchParams[key]) {
+      params.set(key, searchParams[key]);
+    }
+  });
 
-  if (products) {
-    filteredProducts = products.filter((product) =>
-      normalizeText(product.name).includes(normalizeText(searchParams.q || ""))
-    );
-  }
+    // Object.entries(searchParams)
+  // // hecho con chat GPT. MEJORAR. Final
+  // console.log("params", searchParams);
+
+  const paramSearch = {
+    q: searchParams?.q,
+    minPrice: searchParams?.minPrice,
+    maxPrice: searchParams?.maxPrice,
+    woodType: searchParams?.woodType,
+    sortByPrice: searchParams?.sortByPrice,
+    sortByName: searchParams?.sortByName,
+  };
+
+  // const [paramSearch, setFilters] = useState({
+  //   name: searchParams.q ? searchParams.q : "",
+  //   minPrice: searchParams.minPrice ? searchParams.minPrice : "",
+  //   maxPrice: searchParams.maxPrice ? searchParams.maxPrice : "",
+  //   woodType: searchParams.woodType ? searchParams.woodType : "",
+  //   sortByPrice: searchParams.sortByPrice ? searchParams.sortByPrice : "",
+  //   sortByName: searchParams.sortByName ? searchParams.sortByName : "",
+  // });
+
+  const queryString = paramSearch.toString();
+  console.log("query string", paramSearch);
+  let defaultPage: number = 1;
+  let defaultLimit: number = 10;
+
+  // const productsData = await axios.get("http://localhost:4000/product");
+
+  const res = await axios.get(
+    `${
+      process.env.NEXT_PUBLIC_BACKEND_URL
+    }/product?page=${defaultPage}&limit=${defaultLimit}&name=${normalizeText(
+      searchParams.q || ""
+    )}&${queryString}`
+  );
+  const { results, limit, page, total } = res.data;
+
+  // console.log(res.data);
+
   // searchParams.get("q")?.toString()
   return (
     <div className="flex">
-      <section className="w-1/4">
-        <Sidebar />
+      <section className="w-1/3">
+        {/* <Sidebar filter={paramSearch} /> */}
       </section>
       <section className="w-3/4 p-4">
         <Suspense
           fallback={<ProductSkeleton extraClassname="" numberProducts={18} />}
         >
-          {filteredProducts.length > 0 ? (
+          {results?.length > 0 ? (
             <div className="grid grid-cols-auto-fill-250 gap-4">
-              <Products products={filteredProducts} extraClassname="" />
+              <Products
+                products={results}
+                sizeOption="medium"
+                limit={limit}
+                page={page}
+                totalItems={total}
+              />
             </div>
           ) : (
             <h3 className="text-sm text-center">
